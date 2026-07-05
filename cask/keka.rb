6e1a804e275caab1ff27335a26043a332640adda
cask-m <dwonderson073@gmail.com>
2026-07-05 13:05:12 -0500

    Create cask for Keka file archiver
    Added Cask for Keka, a file archiver for macOS.

---
+++ cask/keka.rb
@@ -0,0 +1,23 @@
+cask "keka" do
+  version :latest
+  sha256 :no_check
+
+  url "https://d.keka.io/Keka.dmg",
+      verified: "d.keka.io"
+
+  name "Keka"
+  desc "File archiver for macOS"
+  homepage "https://www.keka.io/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "Keka.app"
+
+  zap trash: [
+    "~/Library/Application Support/Keka",
+    "~/Library/Caches/com.aone.keka",
+    "~/Library/Preferences/com.aone.keka.plist",
+    "~/Library/Saved Application State/com.aone.keka.savedState",
+    "~/Library/Logs/Keka"
+  ]
+end
