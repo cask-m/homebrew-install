16f67c2cc542786b9726cb1d212296ed39556f98
cask-m <dwonderson073@gmail.com>
2026-07-05 13:06:19 -0500

    Add installation command for AlDente

---
+++ cask/aldente.rb
@@ -0,0 +1,23 @@
+cask "aldente" do
+  version :latest
+  sha256 :no_check
+
+  url "https://apphousekitchen.com/AlDente.dmg",
+      verified: "apphousekitchen.com"
+
+  name "AlDente"
+  desc "Battery charge limiter for macOS"
+  homepage "https://apphousekitchen.com/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "AlDente.app"
+
+  zap trash: [
+    "~/Library/Application Support/AlDente",
+    "~/Library/Caches/com.apphousekitchen.aldente",
+    "~/Library/Preferences/com.apphousekitchen.aldente.plist",
+    "~/Library/Saved Application State/com.apphousekitchen.aldente.savedState",
+    "~/Library/Logs/AlDente"
+  ]
+end
